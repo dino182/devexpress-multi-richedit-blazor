8072a4c2a47cd1ce2f8bf89b7f70b1d77f9e6ee2
Dino Infantino <dino.infantino@displayplan.com>
2021-10-27 21:13:41 +0100

    Add demo code

---
+++ WebApplication/Client/Shared/DataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication.Client.Shared
+{
+    public class DataModel
+    {
+        public string DocumentContent { get; set; }
+
+        public string DocumentName { get; set; }
+
+        public DataView Component { get; set; }
+    }
+}
